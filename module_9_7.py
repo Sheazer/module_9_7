9454ac531264a76683e21d6927e3d1ef6dbbbb5a
Sheazer <57393800+Sheazer@users.noreply.github.com>
2024-10-19 21:35:35 +0600

    Update module_9_7.py

--- module_9_7.py
+++ module_9_7.py
@@ -1,18 +1,21 @@
 def is_prime(func):
-    def surrogate(a, b, c):
+    def wrapper(a, b, c):
         result = func(a, b, c)
+        if result < 2:
+            print('Простое число должно быть больше 1')
+            return result
         for i in range(2, result):
             if result % i == 0:
                 print('Составное')
                 return result
         print('Простое')
         return result
-    return surrogate
+    return wrapper
 
 
 @is_prime
 def sum_three(a,b,c):
     return a+b+c
 
-result = sum_three(2, 3, 6)
+result = sum_three(0, 0, -6)
 print(result)
